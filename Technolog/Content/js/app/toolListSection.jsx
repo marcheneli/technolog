4002350a089ba98dcef692ed3a239d9bfece7e62
marcheneli <marcheneli@gmail.com>
2016-02-20 22:21:06 +0300

    Add choose tool option

--- Technolog/Content/js/app/toolListSection.jsx
+++ Technolog/Content/js/app/toolListSection.jsx
@@ -1,9 +1,52 @@
-﻿var ToolList = React.createClass({
+﻿var ToolListRow = React.createClass({
+    getInitialState: function() {
+        return {
+            color: this.props.isCurrent ? 'info' : null
+        }
+    },
+    onMouseEnterHandler: function() {
+        if(!this.props.isCurrent) this.setState({ color: "active"});
+    },
+    onMouseLeaveHandler: function() {
+        if(!this.props.isCurrent) this.setState({ color: null});
+    },
+    onClickHandler: function() {
+        if(!this.props.isCurrent) {this.setState({ color: null}); this.props.changeCurrent(this.props.tool); }
+        else {this.setState({ color: "active"}); this.props.changeCurrent(null); }
+    },
+    render: function() {
+        return(
+            <tr className={this.props.isCurrent ? 'info' : this.state.color}
+                onMouseEnter={this.onMouseEnterHandler}
+                onMouseLeave={this.onMouseLeaveHandler}
+                onClick={this.onClickHandler}>
+                <td>{this.props.tool.id}</td>
+                <td>{this.props.tool.name}</td>
+            </tr>
+        )
+    }
+});
+var ToolList = React.createClass({
+    getInitialState: function() {
+        return {
+            currentTool: null
+        }
+    },
+    changeCurrent: function(tool) {
+        this.setState({ currentTool: tool });
+    },
     render: function() {
         return(
             <div className="panel panel-default full-height" style={{width: 600 + 'px', marginBottom: 0 + 'px'}}>
-                <div className="panel-heading">Инструменты</div>
+                <div className="panel-heading">
+                    <h4>Инструменты</h4>
+                </div>
                 <div className="panel-body">
+                    <div className="btn-group" role="group" aria-label="...">
+                        <button className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+                        <button className="btn btn-default"><span className="glyphicon glyphicon-trash"></span></button>
+                        <button className="btn btn-default"><span className="glyphicon glyphicon-refresh"></span></button>
+                    </div>
                     <table className="table table-bordered">
                         <thead>
                             <tr>
@@ -14,15 +57,13 @@
                         <tbody>
                             {this.props.tools.map(function(tool, index) {
                                 return(
-                                    <tr key={index}>
-                                        <td>{tool.id}</td>
-                                        <td>{tool.name}</td>
-                                    </tr>
+                                                <ToolListRow key={tool.id} tool={tool} isCurrent={this.state.currentTool == tool} changeCurrent={this.changeCurrent} />
                                 )
-                            })}
+                            }.bind(this))}
                         </tbody>
                     </table>
                 </div>
+                
             </div>
         )
     }
